9ad023d309d98d33931f60f3124bd897d34ca240
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2023-03-22 22:46:51 +0200

    Add basic Spring Boot App. Closes #3

---
+++ online-shop-server/src/main/java/com/yashmerino/online/shop/server/OnlineShopServerApplication.java
@@ -0,0 +1,13 @@
+package com.yashmerino.online.shop.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopServerApplication.class, args);
+	}
+
+}
