4963e7fca2d24a1ee435fe163dbccc0bcc9cf367
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2023-03-22 22:46:51 +0200

    Add basic Spring Boot App. Closes #3

---
+++ online-shop-server/src/test/java/com/yashmerino/online/shop/server/OnlineShopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashmerino.online.shop.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
