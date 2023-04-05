a21c044290232aa9bb6a4a4576cbfce11cd795aa
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2023-04-05 22:44:02 +0300

    Use TypeScript for UI

--- online-shop-ui/src/index.tsx
+++ online-shop-ui/src/index.tsx
@@ -22,13 +22,14 @@
  * SOFTWARE.
  */
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./main.scss";
-import reportWebVitals from "./reportWebVitals";
-import App from "./app/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './ap   p/App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
 root.render(
     <React.StrictMode>
         <App />
--- online-shop-ui/src/setupTests.ts
+++ online-shop-ui/src/setupTests.ts
@@ -22,10 +22,8 @@
  * SOFTWARE.
  */
 
-import React from 'react';
-
-const App = () => {
-    return <h1>Hello, World!</h1>;
-}
-
-export default App;
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
