a592ba3d26a27e185fc7fc96eb0a09e6d4465c2f
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2023-04-09 22:10:17 +0300

    Add initializer

---
+++ online-shop-server/src/main/java/com/yashmerino/online/shop/Initializer.java
@@ -0,0 +1,13 @@
+package com.yashmerino.online.shop;
+
+import org.springframework.boot.CommandLineRunner;
+
+/**
+ * Class that initializes data.
+ */
+public class Initializer implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
