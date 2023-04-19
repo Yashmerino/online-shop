479932332084e07bc4b6f21bf1155d60bfd680ad
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2023-04-19 21:31:16 +0300

    Fix test

--- online-shop-server/src/test/java/com/yashmerino/online/shop/security/SecurityTest.java
+++ online-shop-server/src/test/java/com/yashmerino/online/shop/security/SecurityTest.java
@@ -28,6 +28,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -39,6 +40,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class SecurityTest {
 
     /**
