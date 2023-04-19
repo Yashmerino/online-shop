4f943289a9d3b7ee6a38b5829a1e9f2b40542539
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2023-04-19 18:45:57 +0300

    Add security tests

---
+++ online-shop-server/src/test/java/com/yashmerino/online/shop/security/SecurityTest.java
@@ -0,0 +1,60 @@
+package com.yashmerino.online.shop.security;
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ + MIT License
+ +
+ + Copyright (c) 2023 Artiom Bozieac
+ +
+ + Permission is hereby granted, free of charge, to any person obtaining a copy
+ + of this software and associated documentation files (the "Software"), to deal
+ + in the Software without restriction, including without limitation the rights
+ + to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ + copies of the Software, and to permit persons to whom the Software is
+ + furnished to do so, subject to the following conditions:
+ +
+ + The above copyright notice and this permission notice shall be included in all
+ + copies or substantial portions of the Software.
+ +
+ + THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ + IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ + FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ + AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ + LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ + OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ + SOFTWARE.
+ +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/**
+ * Tests for security part of the application.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class SecurityTest {
+
+    /**
+     * MVC Mock.
+     */
+    @Autowired
+    private MockMvc mvc;
+
+    /**
+     * Test that any endpoint and request that is not specified in {@link SecurityConfig}
+     * is secured and throws unauthorized it requested without JWT Token.
+     *
+     * @throws Exception if something goes wrong.
+     */
+    @Test
+    void anyRequestIsSecuredTest() throws Exception {
+        mvc.perform(delete("/api/auth/register")).andExpect(status().isUnauthorized());
+    }
+}
