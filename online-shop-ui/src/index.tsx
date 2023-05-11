bebcf371ecb3a360d18db2b1f00c620ab31272b5
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2023-05-11 22:37:17 +0300

    Setup Redux. Closes #52

--- online-shop-ui/src/index.tsx
+++ online-shop-ui/src/index.tsx
@@ -31,13 +31,19 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import store from "./app/store";
+import { Provider } from "react-redux";
+
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
