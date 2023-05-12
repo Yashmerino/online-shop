e9aff43a8fdefca45485fb198cb84a4ffc0709db
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2023-05-12 23:29:28 +0300

    Remove duplications

--- online-shop-ui/src/app/components/pages/RegisterPage.tsx
+++ online-shop-ui/src/app/components/pages/RegisterPage.tsx
@@ -1,90 +1,30 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
 import Copyright from '../footer/Copyright';
+import UserInputFields from './UserInputFields';
 import { Link as RouterLink } from 'react-router-dom';
 
 const RegisterPage = () => {
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            username: data.get('username'),
-            password: data.get('password'),
-        });
     };
 
     return (
         <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
-            >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Create an account
-                </Typography>
-                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="username"
-                        label="Username"
-                        name="username"
-                        autoComplete="username"
-                        autoFocus
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
-                        Register
-                    </Button>
-                    <Grid container>
-                        <Grid item>
-                            <Link component={RouterLink} to={'/login'} variant="body2">
-                                {"Have already an account? Log in"}
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Box>
+            <Grid container>
+                <Grid item>
+                    <UserInputFields title="Sign Up" buttonText="Sign Up" handleSubmit={handleSubmit} />
+                </Grid>
+                <Grid item>
+                    <Link component={RouterLink} to={'/login'} variant="body2">
+                        {"Have already an account? Log in"}
+                    </Link>
+                </Grid>
+            </Grid>
             <Copyright sx={{ mt: 8, mb: 4 }} />
         </Container>
     );
---
+++ online-shop-ui/src/app/components/pages/UserInputFields.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+
+interface UserInputProps {
+    title: string,
+    buttonText: string,
+    handleSubmit: Function
+}
+
+const UserInputFields = ({ title, buttonText, handleSubmit }: UserInputProps) => {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    {title}
+                </Typography>
+                <Box component="form" onSubmit={() => handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="username"
+                        label="Username"
+                        name="username"
+                        autoComplete="username"
+                        autoFocus
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        {buttonText}
+                    </Button>
+                </Box>
+            </Box>
+        </React.Fragment>);
+}
+
+export default UserInputFields;
