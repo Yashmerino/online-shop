aaaf6513c462872d8ad9d9e0d08f2243d159d5f5
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2023-05-23 19:05:42 +0300

    #65 Add test cases for request errors

--- online-shop-ui/src/__tests__/LoginPage.test.tsx
+++ online-shop-ui/src/__tests__/LoginPage.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import LoginPage from "../app/components/pages/LoginPage";
 import * as AuthRequest from "../app/api/AuthRequest";
@@ -33,5 +33,41 @@ describe("Login Page Tests", () => {
 
         const submitButton = screen.getByTestId("submit-button");
         fireEvent.click(submitButton);
+    });
+
+    it("Test login fail", async () => {
+        render(
+            <MemoryRouter>
+                <LoginPage />
+            </MemoryRouter>
+        );
+
+        const loginMock = jest.spyOn(AuthRequest, 'login');
+        loginMock.mockReturnValue(Promise.resolve({ status: 404, error: "Error" }));
+
+        const title = screen.getByTestId("title");
+        expect(title).toBeInTheDocument();
+        expect(title).toHaveTextContent("Sign In");
+
+        const usernameInput = screen.getByText("Username");
+        expect(usernameInput).toBeInTheDocument();
+
+        const passwordInput = screen.getByText("Password");
+        expect(passwordInput).toBeInTheDocument();
+
+        const copyright = screen.getByText("Online Shop");
+        expect(copyright).toBeInTheDocument();
+
+        const registerHref = screen.getByText("Don't have an account? Sign Up");
+        expect(registerHref).toBeInTheDocument();
+
+        const submitButton = screen.getByTestId("submit-button");
+        fireEvent.click(submitButton);
+        
+        await waitFor(() => {
+            const alertError = screen.getByTestId("alert-error");
+            expect(alertError).toBeInTheDocument();
+            expect(alertError).toHaveTextContent("Error");
+        });
     });
 });
