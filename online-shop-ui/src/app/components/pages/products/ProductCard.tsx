deb3f110493b59beb6154dd6df3e041b11fa8034
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2023-05-27 17:44:17 +0300

    #66 Add ProductCard component

---
+++ online-shop-ui/src/app/components/pages/products/ProductCard.tsx
@@ -0,0 +1,60 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Artiom Bozieac
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+const ProductCard = () => {
+    return (
+        <Card sx={{ maxWidth: 400, marginTop: "5%" }} className="card-item">
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              alt="product"
+              width="320"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Title
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Price: 5$
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions >
+            <Button size="small" color="primary">
+              Add To Cart
+            </Button>
+          </CardActions>
+        </Card>
+      );
+}
+
+export default ProductCard;
