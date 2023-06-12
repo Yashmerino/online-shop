04e02b82f9a9b4af552164102cd6c30bce15e098
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2023-06-12 19:39:11 +0300

    Make Header button redirecting to Cart. Closes #81

--- online-shop-ui/src/app/components/Header.tsx
+++ online-shop-ui/src/app/components/Header.tsx
@@ -5,16 +5,16 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-const settings = ['Profile', 'My Cart', 'Logout'];
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const navigate = useNavigate();
     const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
     const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -24,6 +24,10 @@ const Header = () => {
     const handleCloseUserMenu = () => {
         setAnchorElUser(null);
     };
+
+    const handleMyCart = () => {
+        navigate("/cart");
+    }
 
     return (
         <AppBar position="static" sx={{ width: "100%" }}>
@@ -91,11 +95,9 @@ const Header = () => {
                             open={Boolean(anchorElUser)}
                             onClose={handleCloseUserMenu}
                         >
-                            {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                    <Typography textAlign="center">{setting}</Typography>
-                                </MenuItem>
-                            ))}
+                            <MenuItem onClick={() => { }}>Profile</MenuItem>
+                            <MenuItem onClick={handleMyCart}>My Cart</MenuItem>
+                            <MenuItem onClick={() => { }}>Logout</MenuItem>
                         </Menu>
                     </Box>
                 </Toolbar>
