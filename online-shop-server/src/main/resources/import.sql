5bc231f28736cd529b903445d90c8b364fed16cc
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2023-08-23 23:16:53 +0300

    #88 Add categories into DB

--- online-shop-server/src/main/resources/import.sql
+++ online-shop-server/src/main/resources/import.sql
@@ -1,3 +1,13 @@
 INSERT INTO roles (id, name) VALUES (1, 'USER');
 INSERT INTO roles (id, name) VALUES (2, 'SELLER');
 INSERT INTO roles (id, name) VALUES (3, 'ADMIN');
+
+INSERT INTO categories (id, name) VALUES (1, 'Digital Services');
+INSERT INTO categories (id, name) VALUES (2, 'Cosmetics and Body are');
+INSERT INTO categories (id, name) VALUES (3, 'Food and Beverage');
+INSERT INTO categories (id, name) VALUES (4, 'Furniture and Decor');
+INSERT INTO categories (id, name) VALUES (5, 'Health and Wellness');
+INSERT INTO categories (id, name) VALUES (6, 'Household Items');
+INSERT INTO categories (id, name) VALUES (7, 'Media');
+INSERT INTO categories (id, name) VALUES (8, 'Pet Care');
+INSERT INTO categories (id, name) VALUES (9, 'Office equipment');
