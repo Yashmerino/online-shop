66cfafc2af116601e058c8923f341936d955bacb
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2023-09-29 18:41:38 +0300

    Redirect user to login page if he's not logged in. Closes #98

--- online-shop-ui/src/app/Main.tsx
+++ online-shop-ui/src/app/Main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './components/pages/auth/LoginPage';
 import RegisterPage from './components/pages/auth/RegisterPage';
 import ProductsContainer from './components/pages/products/ProductsContainer';
@@ -9,18 +9,21 @@ import AddProductPage from './components/pages/products/AddProductPage';
 import MyProductsPage from './components/pages/products/MyProductsPage';
 import MyProfilePage from './components/pages/user/MyProfilePage';
 import EditProductPage from './components/pages/products/EditProductPage';
+import { useAppSelector } from './hooks';
 
 const Main = () => {
+    const jwt = useAppSelector(state => state.jwt.token);
+
     return (
         <Routes>
             <Route path='/login' element={<LoginPage />} />
             <Route path='/register' element={<RegisterPage />} />
-            <Route path='/products' element={<ProductsContainer />} />
-            <Route path='/cart' element={<CartContainer />} />
-            <Route path='/product/add' element={<AddProductPage />} />
-            <Route path='/profile/products' element={<MyProductsPage />} />
-            <Route path='/profile' element={<MyProfilePage />} />
-            <Route path='/product/edit' element={<EditProductPage />} />
+            <Route path='/products' element={jwt.length > 0 ? <ProductsContainer /> : <Navigate to='/login' />} />
+            <Route path='/cart' element={jwt.length > 0 ? <CartContainer /> : <Navigate to='/login' />} />
+            <Route path='/product/add' element={jwt.length > 0 ? <AddProductPage /> : <Navigate to='/login' />} />
+            <Route path='/profile/products' element={jwt.length > 0 ? <MyProductsPage /> : <Navigate to='/login' />} />
+            <Route path='/profile' element={jwt.length > 0 ? <MyProfilePage /> : <Navigate to='/login' />} />
+            <Route path='/product/edit' element={jwt.length > 0 ? <EditProductPage /> : <Navigate to='/login' />} />
         </Routes>
     );
 }
