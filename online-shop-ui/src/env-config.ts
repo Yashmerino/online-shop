062e2e1ba12b8de770c6dc7f3af38ebd5ffbc5dc
Artiom Bozieac <artiomytprofile@gmail.com>
2024-02-26 16:50:27 +0200

    Update env-config.ts

--- online-shop-ui/src/env-config.ts
+++ online-shop-ui/src/env-config.ts
@@ -3,4 +3,4 @@ export const JWT_SECRET = "640762F165320F52408DAFED313C106346575273C66013DE94B8D
 export const ALGOLIA_APP_ID = "KLFWXPOEHY";
 export const ALGOLIA_API_KEY = "87c939ac9269c88a17beeaacca28567a";
 export const ALGOLIA_INDEX_NAME = "online-shop-dev";
-export const ALGOLIA_USAGE = false;
+export const ALGOLIA_USAGE = true;
